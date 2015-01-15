7954fe1cdea2ca0ec5075d4baee7eb76248dbf61
Tommaso Soru <mommi84@gmail.com>
2015-01-15 16:05:00 +0100

    add app metadata

---
+++ src/org/aksw/tsoru/textmining/AddMeta.java
@@ -0,0 +1,80 @@
+package org.aksw.tsoru.textmining;
+
+import io.LoadData;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import data.AndroidApp;
+import data.Review;
+
+/**
+ * @author Tommaso Soru <t.soru@informatik.uni-leipzig.de>
+ *
+ */
+public class AddMeta {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		HashMap<String, AndroidApp> appdata = new HashMap<String, AndroidApp>();
+		ArrayList<AndroidApp> data = LoadData.loadData("data/", true);
+		for(AndroidApp app : data) {
+			appdata.put(app.getId(), app);
+		}
+		System.out.println("============");
+		
+		Scanner in = new Scanner(new File("output.csv"));
+		PrintWriter pw = new PrintWriter(new File("metadata.csv"));
+		
+		in.next(); // skip titles
+		pw.write("APP_ID,CATEGORY,RATING\n");
+		
+		while(in.hasNextLine()) {
+			
+			String line;
+			try {
+				line = in.next();
+			} catch (NoSuchElementException e1) {
+				// end of file
+				break;
+			}
+			
+			String[] app = line.split(",");
+			String appID = app[0];
+			
+			AndroidApp a = appdata.get(appID);
+			
+			if(a == null)
+				continue;
+			
+			String cat = a.getCategory();
+			
+			double sum = 0.0;
+			for(Review r : a.getReviews())
+				try {
+					sum += Double.parseDouble(r.getRating());
+				} catch (NumberFormatException e) {
+					// HTML string instead of raw value
+					double d = Double.parseDouble(r.getRating().split("%")[0]);
+					System.out.println(r.getRating() + " is "+d);
+					sum += d;
+				}
+			
+			String rating = "" + (sum / a.getReviews().size());
+			
+			String string = appID+","+cat+","+rating+","+"\n";
+			pw.write(string);
+			
+		}
+		
+		in.close();
+		pw.close();
+		
+	}
+
+}
